3089ebc214e400c11a788334be3b55b87abd10d8
Viktor Laishev <130911518+ViktorLaishev@users.noreply.github.com>
2023-05-13 16:35:49 +0200

    Update KdTree.java

--- KdTree.java
+++ KdTree.java
@@ -1,6 +1,6 @@
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;   
-import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdDraw; 
   
 import java.util.Stack;  
  
